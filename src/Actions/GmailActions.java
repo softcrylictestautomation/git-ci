7374b9d66ef0c61a17237686663f17fc0f6f407c
automateon <skhana@softcrylic.co.in>
2013-09-23 13:28:14 -0400

    Update GmailActions.java
    testing the demo

--- src/Actions/GmailActions.java
+++ src/Actions/GmailActions.java
@@ -28,7 +28,7 @@ public class GmailActions {
 		try {
 			// Look for search textbox and enter search term there
 			WebElement searchBox = driver.findElement(By.name("q"));
-			searchBox.sendKeys("WebDriver API");
+			searchBox.sendKeys("Appium API");
 			logger.createTestStepWithImage("Type in WebDriver API", "Sucess",
 					"Sucess", true);
 		} catch (Exception e) {
